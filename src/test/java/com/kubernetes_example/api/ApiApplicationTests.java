91a7341eb8f0a8bc67a3ad4436b1ae1bf0ed9a9f
cessadev <cesarurb.dev@gmail.com>
2024-09-13 13:44:02 -0500

    first commit

---
+++ src/test/java/com/kubernetes_example/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes_example.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
